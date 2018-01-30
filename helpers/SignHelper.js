a78665b85321930b6172e8bddb230c469192c203
Roman A. Sarria <agustin478@gmail.com>
2018-01-30 13:35:00 -0300

    Fix on how the string is encrypted

--- helpers/SignHelper.js
+++ helpers/SignHelper.js
@@ -1,5 +1,5 @@
-var util = require('util');
-var spawn = require('child_process').execSync;
+var util = require('util'),
+	exec = require('child_process').exec;
 
 // Expose methods.
 exports.sign = sign;
@@ -15,34 +15,35 @@ exports.sign = sign;
  */
 
 function sign(options) {
-    return new Promise(function (resolve, reject) {
-        options = options || {};
+	return new Promise(function (resolve, reject) {
+		options = options || {};
 
-        if (!options.content)
-            reject('Invalid content.');
+		if (!options.content)
+			reject('Invalid content.');
 
-        if (!options.key)
-            reject('Invalid key.');
+		if (!options.key)
+			reject('Invalid key.');
 
-        if (!options.cert)
-            reject('Invalid certificate.');
+		if (!options.cert)
+			reject('Invalid certificate.');
 
-        var command = util.format(
-            'echo "%s" | openssl smime -sign -signer %s -inkey %s -outform DER -nodetach',
+		var command = util.format(
+			'echo "%s" | openssl smime -sign -signer %s -inkey %s -outform DER -nodetach',
 			options.content.replace(/["']/g, '\\"'),
-            options.cert,
-            options.key
-        );
+			options.cert,
+			options.key
+		);
 
-        if (options.password)
-            command += util.format(' -passin pass:%s', options.password);
+		if (options.password)
+			command += util.format(' -passin pass:%s', options.password);
 
-		//console.info(command);
 
-        var child = spawn(command);
-		
-		var enc = child.toString('base64');
-
-        resolve(enc);
-    });
+		exec(command, { encoding: 'base64' }, (error, stdout, stderr) => {
+			if (error || stderr) {
+				reject(stderr);
+			} else {
+				resolve(stdout);
+			}
+		});
+	});
 }
