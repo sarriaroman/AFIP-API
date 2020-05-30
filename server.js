cd75ae35d51d99d5566d1e271b3d9b5db021425e
Santiago Quinteros <contacto.quinteros@gmail.com>
2020-05-30 14:34:46 -0300

    Change listen to start in order to work with restana

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ global.keys = {
 	private: path.join(__dirname, 'keys', 'afip.key'),
 	public: path.join(__dirname, 'keys', 'afip.pem')
 };
+(async function () {
+	// Lets create Restana
+	// This is the HTTP Version check next links to upgrde to HTTPS or even HTTP/2
+	// HTTPS: https://github.com/jkyberneees/ana/blob/master/demos/https-service.js
+	// HTTP/2: https://github.com/jkyberneees/ana/blob/master/demos/http2-service.js
+	const app = restana();
 
-// Lets create Restana
-// This is the HTTP Version check next links to upgrde to HTTPS or even HTTP/2
-// HTTPS: https://github.com/jkyberneees/ana/blob/master/demos/https-service.js
-// HTTP/2: https://github.com/jkyberneees/ana/blob/master/demos/http2-service.js
-const app = restana();
+	// Start Routes
+	index(app, false);
 
-// Start Routes
-index(app, false);
+	await app.start(process.env.PORT || 5000);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('AFIP API Corriendo en el puerto ' + ( process.env.PORT || 3000 ));
-});
+	console.log(
+		"AFIP API Corriendo en el puerto " +
+			(process.env.PORT || 5000)
+	);
+})()
